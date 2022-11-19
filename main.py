2c4ac99cbf9632d3df534f8d88f27197b72ec91c
Terbik <terbik44@gmail.com>
2022-11-19 21:53:24 +0300

    no bar, project is done

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import requests
 from Settings import TOKEN_VK, TOKEN_YANDEX
 from pprint import pprint
+from progress.bar import IncrementalBar
+# from tqdm import tqdm
+import time
+
 
 class VK:
     base_host = 'https://api.vk.com/method'
@@ -24,18 +28,24 @@ class VK:
 
     def photos_get(self):
         url = '/photos.get'
+
         respoun = requests.get(self.base_host + url, params=self.Get_params()).json()
-        pprint(respoun)
+        # pprint(respoun)
         return respoun
 
     def transform_dict_vk(self):
         new_dict = VK.photos_get(self)
         reforge_dict = []
-        new = len(new_dict['response']['items'])
-        for count in range(0, len(new_dict['response']['items'])):
-            reforge_dict.append((VK.photos_get(self)['response']['items'][int(count)]['sizes'][-1]['url'], VK.photos_get(self)['response']['items'][int(count)]['likes']['count']))
 
-        pprint(reforge_dict)
+        bar = IncrementalBar('---', max=len(new_dict['response']['items']))
+        for count in range(0, len(new_dict['response']['items'])):
+            bar.next()
+            reforge_dict.append((new_dict['response']['items'][count]['sizes'][-1]['url'],
+                                 new_dict['response']['items'][count]['likes']['count']))
+
+        bar.finish()
+
+        # pprint(reforge_dict)
         return reforge_dict
 
 
@@ -54,18 +64,26 @@ class Yandex_disk():
         }
 
 
-    def upload_from_internet(self, dict_url):
-        uri = 'v1/disk/resources/upload/new_vk'
-        request_url = self.base_host + uri
-        for item in dict_url:
-            params = {'url': item[0], 'path': f'/{item[1]}.jpg'}
-            response = requests.post(request_url, params=params, headers=self.get_headers_authorization())
-            print(response.status_code)
-            print(response.json())
+    def upload_from_internet(self, reforge_dict):
+        folder_name = input("Дайте название папки, в которой хотите хранить фото БЕЗ ПРОБЕЛОВ: ")
+        uri_uploud = 'v1/disk/resources/upload'
+        uri_folder = 'v1/disk/resources'
+        path = {'path': folder_name}
+
+        check_folder = requests.get(self.base_host + uri_folder, params=path)
+        if check_folder.status_code == 401:
+            folder = requests.put(self.base_host + uri_folder, params=path, headers=self.get_headers_authorization())
+            # print(folder.status_code)
+            # print(folder.json())
+        for item in reforge_dict:
+            params = {'url': item[0], 'path': f'/{folder_name}/{item[1]}.jpg'}
+            response = requests.post(self.base_host + uri_uploud, params=params, headers=self.get_headers_authorization())
+            # print(response.status_code)
+            # print(response.json())
+        print('Все готово!')
 
 
 if __name__ == '__main__':
     yandex = Yandex_disk(TOKEN_YANDEX)
     vk = VK(TOKEN_VK)
-    vk.photos_get()
     yandex.upload_from_internet(vk.transform_dict_vk())
